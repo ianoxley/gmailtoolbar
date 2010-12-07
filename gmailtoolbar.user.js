d3c94070fe022813b0ed73d6befccb71fc6ee094
ianoxley <ijoxley@gmail.com>
2010-12-07 23:11:36 +0000

    Changed CSS selector to target the correct <div> tags

--- gmailtoolbar.user.js
+++ gmailtoolbar.user.js
@@ -1,26 +1,25 @@
 // ==UserScript==
-// @name           	gmailtoolbar
-// @namespace      	http://www.strongasanox.co.uk/greasemonkey
-// @description    	Gives the GMail toolbar at the top of GMail fixed positioning
-// @match        		http://mail.google.*
+// @name          GMail Toolbar
+// @namespace     http://www.strongasanox.co.uk/greasemonkey
+// @description		Gives the GMail toolbar at the top of GMail fixed positioning
+// @match        	http://mail.google.com/*
+// @match        	https://mail.google.com/*
 // ==/UserScript==
 (function() {
-	window.addEventListener('load', function() {
-		var css = [
-			'body > div > div > div > div > div.nH > div > div[role="navigation"] {',
-				'background:rgba(26, 32, 34, 0.9) !important;',
-				'display:block;',
-				'left:0;',
-				'position: fixed;',
-				'top:0;',
-				'width:100%;',
-				'z-index:1000;',
-			'}',
-			'body > div > div > div > div > div.nH > div:last-child {',
-			'	margin-top:24px;',
-			'}'
-		];
-		
-		GM_addStyle(css.join(''));
-	}, true);
+	var css = [
+		'body.cP > div > div.nH > div.nH > div.aC > div.nH > div.nH:nth-child(2) {',
+			'background:rgba(26, 32, 34, 0.9) !important;',
+			'display:block;',
+			'left:0;',
+			'position: fixed;',
+			'top:0;',
+			'width:100%;',
+			'z-index:1000;',
+		'}',
+		'body.cP > div > div.nH > div.nH > div.aC > div.nH > div:last-child {',
+		'	margin-top:26px;',
+		'}'
+	];
+	
+	GM_addStyle(css.join(''));
 })();
